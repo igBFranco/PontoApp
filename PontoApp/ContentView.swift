b9de72c312edeea131a09eefa75193fb52a273d9
Igor Bueno Franco <88065589+igBFranco@users.noreply.github.com>
2024-09-20 19:37:22 -0300

    Initial Commit

---
+++ PontoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PontoApp
+//
+//  Created by Igor Bueno Franco on 20/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PontoApp/PontoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PontoAppApp.swift
+//  PontoApp
+//
+//  Created by Igor Bueno Franco on 20/09/24.
+//
+
+import SwiftUI
+
+@main
+struct PontoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
