e033b9ea6624c54e45664ffd37e8ff07bea2e4fc
Igor Bueno Franco <88065589+igBFranco@users.noreply.github.com>
2024-10-14 10:58:19 -0300

    Adding Login and Basic Function

---
+++ PontoApp/RecordView.swift
@@ -0,0 +1,18 @@
+//
+//  RecordView.swift
+//  PontoApp
+//
+//  Created by Igor Bueno Franco on 13/10/24.
+//
+
+import SwiftUI
+
+struct RecordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecordView()
+}
